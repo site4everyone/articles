5b6a67eb3aeead615d53eace51c07b59a86d5ba5
site4everyone <68636587+site4everyone@users.noreply.github.com>
2022-09-05 07:58:25 +0530

    Update views.py

--- search/views.py
+++ search/views.py
@@ -2,15 +2,12 @@ from django.shortcuts import render
 from .models import Article
 # views of search app
 def home(request):
-  query = ''
-  try:
-    query=request.GET['keyword']
-  except:
-    pass
+  query=request.GET.get('keyword', None)
   
-  if query != '':
-    lst = Article.objects.filter(title=query)
+  if query is not None:
+    lst = Article.objects.filter(title__icontains=query)
   else:
     lst = Article.objects.all()
+  
   
   return render(request, 'index.html', {'articles':lst})
